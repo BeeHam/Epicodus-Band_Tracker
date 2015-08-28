75536486242beb2360ee0adee6c131127aaddd92
Brigham <brigham.thompson@gmail.com>
2015-08-28 10:20:37 -0700

    add app.rb path to index and create inex and layout html

--- spec/venues_spec.rb
+++ spec/venues_spec.rb
@@ -3,4 +3,9 @@ require('spec_helper')
   describe(Venue) do
     it {should have_and_belong_to_many(:bands)}
     it {should validate_presence_of(:name)}
+
+    it("will capitalize the name before saving") do
+      venue_name = Venue.create({:name => "generic"})
+      expect(venue_name.name()).to(eq("Generic"))
+    end
   end
