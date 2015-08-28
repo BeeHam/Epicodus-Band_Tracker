8c97a55404aea514c46557d350fa29bd78339975
Brigham <brigham.thompson@gmail.com>
2015-08-28 10:42:01 -0700

    add path to Bands page

---
+++ spec/integration_spec.rb
@@ -0,0 +1,15 @@
+require('spec_helper')
+
+describe('user funtionality through Band Tracker application', {:type => :feature}) do
+  it('will route to Bands page') do
+    visit('/')
+    click_link('Bands')
+    expect(page).to have_content("Bands")
+  end
+
+  # it('will route to the bands page') do
+  #   visit('/')
+  #   click_link('Venues')
+  #   expect(page).to have_content("Venues")
+  # end
+end
