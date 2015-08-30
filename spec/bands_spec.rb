1f4db0df01dae2671f979691f649eefadc05f3ee
Brigham <brigham.thompson@gmail.com>
2015-08-29 22:18:31 -0700

    replace lists for band and venue info with tables

--- spec/bands_spec.rb
+++ spec/bands_spec.rb
@@ -1,11 +1,11 @@
 require('spec_helper')
 
-  describe(Band) do
-    it {should have_and_belong_to_many(:venues)}
-    it {should validate_presence_of(:name)}
+describe(Band) do
+  it {should have_and_belong_to_many(:venues)}
+  it {should validate_presence_of(:name)}
 
-    it("will capitalize the name before saving") do
+  it("will capitalize the name before saving") do
     band_name = Band.create({:name => "generic"})
     expect(band_name.name()).to(eq("Generic"))
   end
-  end
+end
--- spec/venues_spec.rb
+++ spec/venues_spec.rb
@@ -1,11 +1,11 @@
 require('spec_helper')
 
-  describe(Venue) do
-    it {should have_and_belong_to_many(:bands)}
-    it {should validate_presence_of(:name)}
+describe(Venue) do
+  it {should have_and_belong_to_many(:bands)}
+  it {should validate_presence_of(:name)}
 
-    it("will capitalize the name before saving") do
-      venue_name = Venue.create({:name => "generic"})
-      expect(venue_name.name()).to(eq("Generic"))
-    end
+  it("will capitalize the name before saving") do
+    venue_name = Venue.create({:name => "generic"})
+    expect(venue_name.name()).to(eq("Generic"))
   end
+end
